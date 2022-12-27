c45d834563fe78a88db9d25921d81f5557295702
gaelmap <gael@kinshasadigital.com>
2022-12-27 16:24:05 +0200

    feat: add new feature config

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import * as config from 'config';
 async function bootstrap() {
   const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
-
   const port = process.env.PORT || serverConfig.port;
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
 
   await app.listen(port);
 }
